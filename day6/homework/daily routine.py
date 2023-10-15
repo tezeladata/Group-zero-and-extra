a079841add9b1834dc9f7fc22e2970b036895454
tezeladata <david.tezela@gmail.com>
2023-10-15 23:11:47 +0400

    commit

---
+++ day6/homework/daily routine.py
@@ -0,0 +1,12 @@
+cycle_count=0
+while True:
+    print("daily routine:")
+    print("wake up")
+    print("eat food")
+    print("go to school")
+    print("do homework")
+    print("code")
+    print("exercise")
+    cycle_count+=1
+    if cycle_count==7:
+        break
