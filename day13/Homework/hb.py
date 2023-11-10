4a71576edfd296ac3f011c6d5587c2589f9ce9f3
tezeladata <david.tezela@gmail.com>
2023-11-10 10:24:59 +0400

    commit

---
+++ day13/Homework/hb.py
@@ -0,0 +1,6 @@
+def happy_birthday():
+    if name=="mate" or name=="Mate":
+        print("Congratulations", name, ". Happy birthday, you now are", age, "years old!")
+name=input("What's your name? ")
+age=int(input("How old are you? "))
+happy_birthday()
