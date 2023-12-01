03d7bfdaa367e7394eaa41f11346ffd525077f8d
tezeladata <david.tezela@gmail.com>
2023-12-01 23:07:58 +0400

    commit

--- codewars/extra_kata7.py
+++ codewars/extra_kata7.py
@@ -108,3 +108,165 @@ def get_drink_by_profession(param):
 #String Templates - Bug Fixing #5
 def build_string(*args):
     return "I like {}!".format(", ".join(args))
+
+#Never visit a . . . !?
+fruit = ["kiwi",
+    "pear",
+    "kiwi",
+    "banana",
+    "melon",
+    "banana",
+    "melon",
+    "pineapple",
+    "apple",
+    "pineapple",
+    "cucumber",
+    "cucumber",
+    "pineapple",
+    "orange",
+    "grape",
+    "orange",
+    "grape",
+    "apple",
+    "grape",
+    "cherry",
+    "pear",
+    "cherry",
+    "pear",
+    "kiwi",
+    "banana",
+    "kiwi",
+    "apple",
+    "melon",
+    "banana",
+    "melon",
+    "pineapple",
+    "melon",
+    "pineapple",
+    "cucumber",
+    "orange",
+    "apple",
+    "orange",
+    "grape",
+    "orange",
+    "grape",
+    "cherry",
+    "pear",
+    "cherry",
+    "pear",
+    "apple",
+    "pear",
+    "kiwi",
+    "banana",
+    "kiwi",
+    "banana",
+    "melon",
+    "pineapple",
+    "melon",
+    "apple",
+    "cucumber",
+    "pineapple",
+    "cucumber",
+    "orange",
+    "cucumber",
+    "orange",
+    "grape",
+    "cherry",
+    "apple",
+    "cherry",
+    "pear",
+    "cherry",
+    "pear",
+    "kiwi",
+    "pear",
+    "kiwi",
+    "banana",
+    "apple",
+    "banana",
+    "melon",
+    "pineapple",
+    "melon",
+    "pineapple",
+    "cucumber",
+    "pineapple",
+    "cucumber",
+    "apple",
+    "grape",
+    "orange",
+    "grape",
+    "cherry",
+    "grape",
+    "cherry",
+    "pear",
+    "cherry",
+    "apple",
+    "kiwi",
+    "banana",
+    "kiwi",
+    "banana",
+    "melon",
+    "banana",
+    "melon",
+    "pineapple",
+    "apple",
+    "pineapple"]
+    
+def sum_digits(number):
+    sm=0
+    for n in list(str(number)):
+        sm+=int(n)
+    return sm
+    
+def subtract_sum(number):
+    result=number-sum_digits(number)
+    if result<100:
+        return fruit[result-1]
+    else:
+        return subtract_sum(result)
+    
+#Return the day
+def whatday(num):
+    days={
+        1: "Sunday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        5: "Thursday",
+        6: "Friday",
+        7: "Saturday"
+    }
+    if 1<=num<=7:
+        return days[num]
+    else:
+        return "Wrong, please enter a number between 1 and 7"
+    
+#Basic Training: Add item to an Array
+websites.append("codewars")
+
+#Area of a Square
+import math
+def square_area(A):
+    r=(A*4)/(2*math.pi)
+    return round(r*r,2)
+
+#Triple Trouble
+def triple_trouble(one, two, three):
+    new=""
+    for i in range(len(one)):
+        new+=one[i]
+        new+=two[i]
+        new+=three[i]
+    return new
+
+#How old will I be in 2099?
+def calculate_age(year_of_birth, current_year):
+    if year_of_birth-1>current_year:
+        return "You will be born in {} years.".format(year_of_birth-current_year)
+    elif year_of_birth==current_year:
+        return "You were born this very year!"
+    elif year_of_birth-1 == current_year:
+        return "You will be born in {} year.".format(year_of_birth-current_year)
+    elif current_year-1 == year_of_birth:
+        return "You are {} year old.".format(current_year-year_of_birth)
+    else:
+        return "You are {} years old.".format(current_year-year_of_birth)
