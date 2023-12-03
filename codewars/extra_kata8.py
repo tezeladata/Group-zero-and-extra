4bb592c81c0d3e6f0c3d57d3910e2f24b5e6ba13
tezeladata <david.tezela@gmail.com>
2023-12-03 21:39:53 +0400

    commit

--- codewars/extra_kata8.py
+++ codewars/extra_kata8.py
@@ -46,3 +46,87 @@ class Ship:
 #Miles per gallon to kilometers per liter
 def converter(mpg):
     return round(mpg * 1.609344 / 4.54609188, 2)
+
+#Incorrect division method
+def divide_numbers(x,y):
+    return x / y
+
+#Holiday VI - Shark Pontoon
+def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
+    if dolphin==True:
+        shark_speed=shark_speed*0.5
+    my_time=pontoon_distance / you_speed
+    shark_time=shark_distance / shark_speed
+    if my_time>shark_time:
+        return "Shark Bait!"
+    else:
+        return "Alive!"
+    
+#Define a card suit
+def define_suit(card):
+    if card[-1]=="C":
+        return "clubs"
+    elif card[-1]=="D":
+        return "diamonds"
+    elif card[-1]=="H":
+        return "hearts"
+    else:
+        return "spades"
+    
+#Do you speak "English"?
+def sp_eng(sentence): 
+    return "english" in sentence.lower()
+
+#SpeedCode #2 - Array Madness
+def array_madness(a,b):
+    new_arr1=[]
+    new_arr2=[]
+    for i in a:
+        new_arr1.append(i**2)
+    for x in b:
+        new_arr2.append(x**3)
+    return sum(new_arr1) > sum(new_arr2)
+
+#No Loops 2 - You only need one
+def check(a, x): 
+    return x in a
+
+#Grasshopper - Combine strings
+def combine_names(first_name, last_name):
+    return "{} {}".format(first_name, last_name)
+
+#Find the Difference in Age between Oldest and Youngest Family Members
+def difference_in_ages(ages):
+    new=(min(ages), max(ages), max(ages)-min(ages))
+    return new
+
+#The falling speed of petals
+def sakura_fall(v):
+    if v>0:
+        return 400/v
+    else:
+        return 0
+    
+#Remove the time
+def shorten_to_date(long_date):
+    numb=long_date.index(",")
+    return long_date[0:numb]
+
+#Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+def remove(s):
+    while s[-1]=="!":
+        s=s[:-1]
+    return s
+
+#Is it a number?
+def is_digit(s):
+    if len(s)==0 or s[0] not in "-+.0123456789":
+        return False
+    for char in s[1:]:
+        if char not in ".0123456789":
+            return False
+    return True
+
+#Regexp Basics - is it a digit?
+def is_digit(n):
+    return n in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
