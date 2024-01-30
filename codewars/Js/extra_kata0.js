33330dc45cacc876a9edc72d2ceae522b472d317
David Tezelashvili <147733566+tezeladata@users.noreply.github.com>
2024-01-30 22:27:13 +0400

    commit

--- codewars/Js/extra_kata0.js
+++ codewars/Js/extra_kata0.js
@@ -61,3 +61,58 @@ function positiveSum(arr) {
   }
   return sum;
 }
+
+// String repeat
+function repeatStr (n, s) {
+  let newStr="";
+  let i=0;
+  while (i<n){
+    newStr+=s;
+    i++;
+  }
+  return newStr;
+}
+
+// Remove First and Last Character
+function removeChar(str){
+  return str.slice(1,-1)
+};
+
+// Square(n) Sum
+function squareSum(numbers){
+  let sum=0;
+  for (let i=0; i<numbers.length; i++){
+    sum+=(numbers[i])**2;
+  }
+  return sum;
+}
+
+// Grasshopper - Summation
+var summation = function (num) {
+  let sum=0;
+  for (let i=1; i<=num; i++){
+    sum+=i;
+  }
+  return sum;
+}
+
+// Function 1 - hello world
+let greet=function(){
+  return "hello world!"
+}
+
+// Convert a String to a Number!
+const stringToNumber = function(str){
+  return Number(str);
+}
+
+// Remove String Spaces
+function noSpace(x){
+  let res="";
+  for (let i=0; i<x.length; i++){
+    if (x[i]!=" "){
+      res+=x[i]
+    }
+  }
+  return res
+}
