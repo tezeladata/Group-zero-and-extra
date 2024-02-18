c67a4df70d97f5312abfb36dac8078777b90246e
David Tezelashvili <147733566+tezeladata@users.noreply.github.com>
2024-02-18 22:54:17 +0400

    commit

--- Day59/Classwork/script.js
+++ Day59/Classwork/script.js
@@ -71,3 +71,43 @@ const leaderVano=new GoaLeaders("Vano", 16, 10)
 const leaderSandro=new GoaLeaders("Sandro", 16, 10)
 //             new-ახალი ობიექტის შექმნა
 console.log(leaderVano.name, leaderSandro.age)
+
+
+
+// for (let i=0; i<20; i++){
+//     if (i==15){
+//         console.log("Hello World")
+//     } else{
+//         console.log(`${i} < 20`)
+//     }
+// }
+let counter=0;
+while (true){
+    if (counter==20){
+        break
+    }
+    counter++;
+    console.log(counter);
+}
+
+let num1=1;
+while (num1<=100){
+    console.log(num1)
+    num1++;
+}
+
+let age=20;
+if (age>15){
+    console.log(`${age}>15`)
+} else if (age==15){
+    console.log(15)
+} else{
+    console.log(`${age}<15`)
+}
+
+console.log(Math.sqrt(-1))
+
+//initialization, condition, updating
+for (let i=0; i<10; i++){
+    console.log(i)
+}
---
+++ Day60/Homework/faq-accordion/script.js
@@ -0,0 +1,35 @@
+document.getElementById("img1").onclick = function() {
+    let text = document.getElementById("p1");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
+
+  document.getElementById("img2").onclick = function() {
+    let text = document.getElementById("p2");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
+
+  document.getElementById("img3").onclick = function() {
+    let text = document.getElementById("p3");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
+
+  document.getElementById("img4").onclick = function() {
+    let text = document.getElementById("p4");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+  }
