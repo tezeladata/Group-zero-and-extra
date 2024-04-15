f7dee8e6df57ac60af3e6b07b10164d8a6ab7668
tezeladata <David.tezela@gmail.com>
2024-04-15 20:30:30 +0400

    commit

---
+++ Day 76/Classwork/main.py
@@ -0,0 +1,61 @@
+# Revising python
+
+# Function name, (, "Text", )
+print("Hello World!")
+
+# All text data should be in quotation marks
+
+# If you miss the quotation marks around the text the computer won't understand your instructions.
+
+# Numbers don't require quotation marks.
+print(1121)
+
+# Computer programs use variables to remember important information, like items in a shopping cart, prices and discounts.
+# Variable - ინფორმაციის საცავი
+# Storage - c disk, Random-access memory (ოპერატიული მეხსიერება - RAM).
+# ოპერატიული მეხსიერება იგივეა, რაც დროებითი მეხსიერება.
+
+x = 10
+# variable, =, variable data
+
+# ყველა გამოსახულება ერთ მნიშვნელობას აბრუნებს:
+x = 10 + 20 + 30
+# საბოლოოდ დაიყვანება ერთ მნიშვნელობამდე: 60
+
+# Ram creates info like: 0x7ffe5367e044 to store data.
+#                        კონკრეტული მისამართი, რომელიც ინახება hex system-ში.
+
+# Following: 5, 5.6, "abc", [1, 2, 3, 4], True    are data - მონაცემი
+
+# კოდის მუშაობის შემდეგ იშლება დროებითი მეხსიერება
+
+# ცვლადი - ინფორმაციის საცავი - იდენთიფიკატორი
+
+# მნიშვნელობა ყოველთვის იწერება ტოლობის მარჯვნივ, ხოლო ინფორმაციის საცავი კი მარცხნივ
+
+y = 5
+# იდენთიფიკატორი, =, მნიშვნელობა
+# = არის მინიჭების ოპერატორი, ინფორმაციის საცავს ანიჭებს მნიშვნელობას
+
+y += 5
+# შემოკლებული მინიჭების ოპერატორი - shorthand operator
+
+# Strings in Python need to be surrounded by quotation marks.
+
+# ნებისმიერი მონაცემი, რომელიც ბრჭყალებშია მოქცეული, არის string
+
+# Statement - განცხადება არის ერთი ხაზი კოდი
+
+# The print() statement is the easiest way to send a value to the screen.
+
+
+
+# Numerical text data: int and float
+# int - integer - მთელი რიცხვი
+# float - ათწილადი
+
+# დეკლარაცია არის დროებით მეხსიერებაში (RAM-ში) ინფორმაციის საცავის - ცვლადის შექმნა.
+
+price = 99
+price = 100
+# Reassigning a value - რედეკლარირება
