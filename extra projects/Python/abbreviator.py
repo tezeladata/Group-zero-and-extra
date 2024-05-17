a15824da1463dbe512e420660188b25cf006b72b
tezeladata <David.tezela@gmail.com>
2024-05-17 13:58:22 +0400

    commit

--- extra projects/Python/abbreviator.py
+++
@@ -1,9 +0,0 @@
-print("Hello, this code abbreviates your name")
-def abbreviation(name):
-    first=name[0]
-    for letter in range(len(name)):
-        if name[letter]==" ":
-            last=name[letter+1]
-    print ("Abbreviation for name:", name, "is:", (first.upper() + "." + last.upper()))
-    print("Thanks for attention!")
-abbreviation(name=input("Enter your name: "))
