55b70d2bd76b4147486f19d79a645e40b61f706e
tezeladata <David.tezela@gmail.com>
2024-05-17 13:19:52 +0400

    commit

--- W3/w3schools_js/template-strings.js
+++
@@ -1,8 +0,0 @@
-// Template strings use `` - backsticks instead of regular ones
-let text=`Hello World!`
-console.log(text)
-// Template Strings allow both single and double quotes inside a string
-console.log(`It is also "the fastest production car ever"`)
-
-// Template strings also allow multiline strings
-
