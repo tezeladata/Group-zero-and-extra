15a65597c675e5051857bcfe287f8d128a2be7ab
tezeladata <David.tezela@gmail.com>
2024-12-22 17:41:33 +0400

    commit

---
+++ Potential controllers/main.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+# took out these controllers from list: "khuberishvili", "grdzelishvili", "motiashvili", "gvritishvili", "tsitlauri"
+# december first winners - abashidze, alavidze, kvaratskhelia
+all_leaders = [
+"kilasonia", "khuskivadze", "vanishvili", "samsonidze", "khmaladze", "gurgenidze", "abramiani", "narimanidze", "svanidze", "vakhtangashvili", "papunashvili", "adeishvili", "razmadze", "kobakhidze", "varazashvili", "arakhamia", "gujabidze", "kochalidze", "qoridze", "tchitadze", "macharashvili", "shavadze"
+]
+
+winners = []
+def lucky_leader(all, res_arr):
+    print("Lucky leader is:\n")
+
+    # Simple timer
+    start = 10
+    while start > 0:
+        print(start)
+        start -= 1
+        time.sleep(1)
+
+    # Choose and store
+    lucky = random.choice(all)
+    all.remove(lucky)
+    res_arr.append(lucky)
+
+    # Print
+    print(f'\n{"-"*(len(lucky)*2)}')
+    print(f"{lucky.upper()}!!!")
+    print(f'{"-"*(len(lucky)*2)}\n\n')
+
+    return
+
+lucky_leader(all_leaders, winners)
+lucky_leader(all_leaders, winners)
+lucky_leader(all_leaders, winners)
+
+# Final
+print(f"All winners are:\n\n{winners[0].upper()}\n{winners[1].upper()}\n{winners[2].upper()}")
